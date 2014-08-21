fef939e7b26108e1542b270f0002fcf7bf3d68ee
Mathias Buus <mathiasbuus@gmail.com>
2014-08-21 18:54:05 +0200

    fix array indentation

--- index.js
+++ index.js
@@ -1,32 +1,7 @@
 var util = require('util')
 
-var last = function(str) {
-  str = str.trim()
-  return str[str.length-1]
-}
-
-var first = function(str) {
-  return str.trim()[0]
-}
-
-var notEmpty = function(line) {
-  return line.trim()
-}
-
-var notEmptyElse = function() {
-  var notNext = false
-  return function(line, i, lines) {
-    if (notNext) {
-      notNext = false
-      return ''
-    }
-    if (lines[i].trim() === '} else {' && (lines[i+1] || '').trim() === '}') {
-      notNext = true
-      return lines[i].replace('} else {', '}')
-    }
-    return line
-  }
-}
+var INDENT_START = /[\{\[]/
+var INDENT_END = /[\}\]]/
 
 module.exports = function() {
   var lines = []
@@ -41,32 +16,24 @@ module.exports = function() {
   var line = function(fmt) {
     if (!fmt) return line
 
-    if (fmt.trim()[0] === '}' && fmt[fmt.length-1] === '{') {
+    if (INDENT_END.test(fmt.trim()[0]) && INDENT_START.test(fmt[fmt.length-1])) {
       indent--
       push(util.format.apply(util, arguments))
       indent++
       return line
     }
-    if (fmt[fmt.length-1] === '{') {
+    if (INDENT_START.test(fmt[fmt.length-1])) {
       push(util.format.apply(util, arguments))
       indent++
       return line
     }
-    if (fmt.trim()[0] === '}') {
+    if (INDENT_END.test(fmt.trim()[0])) {
       indent--
       push(util.format.apply(util, arguments))
       return line
     }
 
     push(util.format.apply(util, arguments))
-    return line
-  }
-
-  line.trim = function() {
-    lines = lines
-      .filter(notEmpty)
-      .map(notEmptyElse())
-      .filter(notEmpty)
     return line
   }
 
